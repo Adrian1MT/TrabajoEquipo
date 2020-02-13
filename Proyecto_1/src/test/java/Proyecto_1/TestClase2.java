d6897c9a48ecea7485f9fb222e028c447b911028
admin1 <admin1@HZ352033.H015112.NET>
2020-02-13 14:10:27 +0100

    'mensaje'

--- Proyecto_1/src/test/java/Proyecto_1/TestClase2.java
+++ Proyecto_1/src/test/java/Proyecto_1/TestClase2.java
@@ -5,15 +5,16 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestClase2 {
+	private Clase2 clase = new Clase2();
 	private String resultado;
 	private String resultadoesperado;
 
 	@Test
 	public void test() {
 		resultado="Buenos dias";
-		resultadoesperado="Buenos dias";
+		//resultadoesperado=clase.Wellcome();
 		
-		assertSame(resultado,resultadoesperado);
+		//assertSame(resultado,resultadoesperado);
 		
 		//org.junit.Assert.assertEquals(resultado,resultadoesperado);
 	}
