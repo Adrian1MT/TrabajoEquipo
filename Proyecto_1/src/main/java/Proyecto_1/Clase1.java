ce0ce635e58c77654432998df31db8cf5de945c7
IN1DM3_15 <IN1DM3_15@H015112.NET>
2020-02-19 12:03:18 +0100

    mensaje

---
+++ Proyecto_1/src/main/java/Proyecto_1/Clase1.java
@@ -0,0 +1,7 @@
+package Proyecto_1;
+
+public class Clase1 {
+	public int Cuadrado(int x) {
+		return x+x;
+	}
+}
---
+++ Proyecto_1/src/test/java/Proyecto_1/testClase1.java
@@ -0,0 +1,19 @@
+package Proyecto_1;
+
+import org.junit.Test;
+
+public class testClase1 {
+	private Clase2 clase = new Clase2();
+	private String resultado;
+	private String resultadoesperado;
+
+	@Test
+	public void testcuadrado() {
+		resultado="Buenos dias";
+		//resultadoesperado=clase.Wellcome();
+		
+		//assertSame(resultado,resultadoesperado);
+		
+		//org.junit.Assert.assertEquals(resultado,resultadoesperado);
+	}
+}
